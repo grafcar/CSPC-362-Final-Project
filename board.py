6d08e796c7fb832f817a28b93e3014289c80d181
Grafcar <82640687+grafcar@users.noreply.github.com>
2023-02-21 20:04:45 -0800

    terminal checker

---
+++ board.py
@@ -0,0 +1,9 @@
+
+class board():
+    def __init__(self) -> None:
+        pass
+
+
+class movement():
+    def __init__(self,direction):
+        self.direction = direction
---
+++ pieces.py
@@ -0,0 +1,5 @@
+class pieces:
+    def __init__(self,owner):
+        self.owner = owner
+
+    
